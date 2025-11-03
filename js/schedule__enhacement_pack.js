c6c843f18ae1af51f64158c6480fd94e94e50d0d
SAMBUYYA <masayyubalfajar@gmail.com>
2025-11-03 16:46:21 +0700

    Penambahan Free Enhacement Pack

---
+++ js/schedule__enhacement_pack.js
@@ -0,0 +1,41 @@
+schedule.push({
+
+    type    : "once", active: false,
+
+    item    : "headhunting_permit",
+    amount  : 1,
+
+    id      : "level_55_free_enhacement_pack_1_headhunting_permit",
+    name    : "Level 55 Free Enhacement Pack<br>(+1 Headhunting Permit)",
+
+    category: "store-free-enhacement-pack"
+    
+});
+
+schedule.push({
+
+    type    : "once", active: false,
+
+    item    : "headhunting_permit",
+    amount  : 2,
+
+    id      : "level_60_free_enhacement_pack_2_headhunting_permit",
+    name    : "Level 60 Free Enhacement Pack<br>(+2 Headhunting Permit)",
+
+    category: "store-free-enhacement-pack"
+    
+});
+
+schedule.push({
+
+    type    : "once", active: false,
+
+    item    : "headhunting_permit",
+    amount  : 2,
+
+    id      : "level_70_free_enhacement_pack_2_headhunting_permit",
+    name    : "Level 70 Free Enhacement Pack<br>(+2 Headhunting Permit)",
+
+    category: "store-free-enhacement-pack"
+    
+});
